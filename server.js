9acfec72005b8ab4454d5b90e50a4f409b613a4c
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2021-09-01 11:19:45 -0400

    server deployment

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ app.post('/image', (req,res) => {
 	.catch(err => res.status(400).json('error updating image count'))
 })
 
-app.listen(3000, () => {
-	console.log('app is running on port 3000')
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app is running on port ${process.env.PORT}`)
 });
 
